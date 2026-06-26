a7a54a51ae644163e3f92153630fdba291353a8d
BoiGuest <BoiGuest@Kiadtisak.boi.go.th>
2026-06-26 14:34:13 +0700

    add javascript and fix menu logo

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,32 @@
+console.log("Hello World");
+
+const myName = "Kai";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+  // h1.addEventListener('click', function(){
+  //   h1.textContent = myName;
+  //   h1.style.backgroundColor = "red";
+  //   h1.style.padding = "5rem";
+  // });
+
+  // current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+
+// mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
