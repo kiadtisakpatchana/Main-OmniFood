2db4b5bbaed279a23d56607b24e15d061472edd2
kiadtisakpatchana <kiadtisakpatchana@gmail.com>
2026-06-27 13:23:21 +0700

    sticky class won't show up

--- JS/script.js
+++ JS/script.js
@@ -5,18 +5,17 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-  // h1.addEventListener('click', function(){
-  //   h1.textContent = myName;
-  //   h1.style.backgroundColor = "red";
-  //   h1.style.padding = "5rem";
-  // });
+// h1.addEventListener('click', function(){
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
 
-  // current year
+// current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
-
 
 // mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
@@ -25,8 +24,34 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
 
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
 
+    //////////////
+    //Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naveigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
